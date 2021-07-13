2c2b649a3fd90a5fa5e328828431fa5fea792610
Dipti R <razdan.dipti@gmail.com>
2021-07-12 20:26:30 -0400

    Final deployment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");//mongodbatlas- try it  mongoose.connect
 const compression = require("compression");
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002; //3002;// cannot work with a static port number //when deploying to heroku
+
 
 const app = express();
 
@@ -15,10 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budgetonoffdb", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//mainly for testing purpose.. need remote db, change to mongo uri
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budgetonoffdb",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
